78270a25836a8a665a8fa0bab60032d59d67e588
tq-jappy <tq-ed@v7.com>
2013-05-26 17:36:19 +0900

    update Main.java

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,5 +1,8 @@
 package sample;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 /**
  * @author t.endo
  */
@@ -8,7 +11,21 @@ public class Main {
     /**
      * @param args
      */
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        // ServletContextHandler context = new ServletContextHandler(
+        // ServletContextHandler.SESSIONS);
+        // context.setContextPath("/");
+
+        WebAppContext context = new WebAppContext();
+        context.setServer(server);
+        context.setContextPath("/gradle-sample");
+        context.setResourceBase("src/main/webapp");
+        context.setClassLoader(Main.class.getClassLoader());
+        server.setHandler(context);
+
+        System.out.println("server starting...");
+        server.start();
+        server.join();
     }
 }
