aed6c4d5efcadfe3b959f99fb33578b81a0819e5
tq-jappy <tq-ed@v7.com>
2013-05-26 17:29:32 +0900

    follow devcenter-gradle

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * @author t.endo
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
